fa0a99dd622e13f33074f47054a919704dbb2038
samanthacsik <samanthacsik@gmail.com>
2020-06-22 21:57:30 -0700

    LTER temp analysis and creation of fig 1

---
+++ code/0_libraries.R
@@ -0,0 +1,27 @@
+##############################
+# libraries
+##############################
+
+# library(bbmle) # maximum likelihood estiamtion used for FR analyses
+# library(boot) # bootstrapping
+# library(broom) # used alonside nls.multstart for tidying up model outputs
+# library(car) # model predictions
+library(chron) # working with dates #####
+# library(corrplot) # correlation matrix
+# library(cowplot) # organizing fig panels & printing plots
+# library(effsize) # calculate effect sizes #####
+# library(frair) # fitting FR
+library(ggridges) # ridgeline plots for displaying LTER temps #####
+# library(gridExtra) # ggMarginal for correlation plots #####
+library(here) # because we love reproducibility #####
+# library(Hmisc) # plotting CI using stat_summary
+# library(kableExtra) # knitting tables
+library(lubridate) # tidyverse friendly dates #####
+# library(minpack.lm) # nonlinear least squares for fitting FR
+library(naniar) # dealing with missing data in LTER time series #####
+# library(nlme) # nonlinear mixed effects models for consumption data
+# library(nls.multstart) # fitting FR
+# library(nlstools) # used alongside nls.multstart for estimating CI of non-linear regression
+# library(nls2) # nonlinear least squares with brute force for fitting FR
+# library(patchwork) # arranging plots
+library(tidyverse) # the true hero - data wrangling & visualization
