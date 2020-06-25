bae6d1d499f6ba23f9b578ceaf5db45883399a5b
samanthacsik <samanthacsik@gmail.com>
2020-06-25 13:48:11 -0700

    added lmerTest to libraries list

--- code/0_libraries.R
+++ code/0_libraries.R
@@ -6,22 +6,23 @@ library(bbmle) # functional response analysis (in Bart's JAGS setup)
 # library(boot) # bootstrapping
 # library(broom) # used alonside nls.multstart for tidying up model outputs
 # library(car) # model predictions
-library(chron) # working with dates #####
+library(chron) # working with dates 
 # library(corrplot) # correlation matrix
 library(cowplot) # organizing fig panels & printing plots
-# library(effsize) # calculate effect sizes #####
+# library(effsize) # calculate effect sizes 
 # library(frair) # fitting FR
 library(ggpubr) # (in Bart's JAGS setup)
-library(ggridges) # ridgeline plots for displaying LTER temps #####
-# library(gridExtra) # ggMarginal for correlation plots #####
-library(here) # because we love reproducibility #####
+library(ggridges) # ridgeline plots for displaying LTER temps 
+# library(gridExtra) # ggMarginal for correlation plots 
+library(here) # because we love reproducibility 
 # library(Hmisc) # plotting CI using stat_summary
 # library(kableExtra) # knitting tables
 library(lme4) # max consumption analysis (in Bart's JAGS setup)
-library(lubridate) # tidyverse friendly dates #####
+library(lmerTest) # max consumption analysis (ended up using this rather than lme4)
+library(lubridate) # tidyverse friendly dates 
 library(MCMCvis) # monte carlo 
 # library(minpack.lm) # nonlinear least squares for fitting FR
-library(naniar) # dealing with missing data in LTER time series #####
+library(naniar) # dealing with missing data in LTER time series 
 # library(nlme) # nonlinear mixed effects models for consumption data
 # library(nls.multstart) # fitting FR
 # library(nlstools) # used alongside nls.multstart for estimating CI of non-linear regression
